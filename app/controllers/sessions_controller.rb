fc726f50a6d134b154f91dc58df83c5f6d5c0805
Alexis La Porte & Amy Liao <pair+aklap.amyhliao@devbootcamp.com>
2015-11-18 11:27:40 -0500

    update create method in sessions to include omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to root_path
+    auth_hash = request.env['omniauth.auth']
+    if session[:user_id]
+      # Means our user is signed in. Add the authorization to the user
+      User.find(session[:user_id]).add_provider(auth_hash)
+      render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
     else
-      flash.now.alert = 'Login failed'
-      render :new
+      # Log him in or sign him up
+      auth = Authorization.find_or_create(auth_hash)
+      # Create the session
+      session[:user_id] = auth.user.id
+      render :text => "Welcome #{auth.user.username}!"
     end
   end
 
